149da7dc98fbd2d6b042b3e430b2fae72c9c6ba5
Rakesh Raj <cgpa10.0@gmail.com>
2022-09-11 21:24:22 +0530

    login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,52 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const loading = ref(false)
+
+definePageMeta({
+  layout: "form-focus",
+});
+
+const form = reactive({
+  email:"",
+  password:""
+})
+
+async function handleLogin(){
+  try{
+    loading.value = true
+    console.log("login button was hit",form)
+  }
+  catch(e){
+    alert("Error logging into the site check for username and password")
+  }
+  finally{
+    loading.value = false
+  }
+}
+</script>
 
 <template>
   <div>
-    This is login page
+    <h2 class="card-title mb-5 ml-2">Login</h2>
+    <FormKit 
+    type="form"
+    submit-label="Login"
+    @submit="handleLogin"
+    v-model="form"
+    >
+    <FormKit 
+    name="email"
+    label="Email address"
+    validation="required|email"
+    />
+
+    <FormKit 
+    type="password"
+    label="password"
+    name="password"
+    validation="required"
+    />
+
+    </FormKit>
   </div>
 </template>
 
